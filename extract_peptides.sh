5ccf075d3bf02c167df77a330f1f28aa8b6b808a
Avril Metcalfe-Roach <armetcal@edu.uwaterloo.ca>
2026-02-05 14:29:29 -0800

    formatting

--- extract_peptides.sh
+++ extract_peptides.sh
@@ -6,19 +6,19 @@
 
 # Example long usage:~~~~
 #DATA_DIRECTORY="/home/armetcal/projects/rrg-bfinlay/armetcal/cmmi/pilot_g4h/mtx_extracted"
-#./extract_peptides.sh \
+# ./extract_peptides.sh \
 #    --working-dir $(pwd) \
-    # --humann-out $DATA_DIRECTORY/humann_out/ur50/mtx \
-    # --targets mtx_uniprot50_prev50_abun1_bug3_IDs.txt \
-    # --fastq-dir $DATA_DIRECTORY/hostile/concatenated \
-    # --output-dir extract_peptides_outputs \
-    # --database /home/armetcal/scratch/humann_databases/uniref50/uniref50_201901b_full.dmnd \
-    # --max-targets 5 \
-    # --evalue 1e-3 \
-    # --pident 90 \
-    # --min-length 7 \
-    # --sensitive \
-    # --rerun
+#     --humann-out $DATA_DIRECTORY/humann_out/ur50/mtx \
+#     --targets mtx_uniprot50_prev50_abun1_bug3_IDs.txt \
+#     --fastq-dir $DATA_DIRECTORY/hostile/concatenated \
+#     --output-dir extract_peptides_outputs \
+#     --database /home/armetcal/scratch/humann_databases/uniref50/uniref50_201901b_full.dmnd \
+#     --max-targets 5 \
+#     --evalue 1e-3 \
+#     --pident 90 \
+#     --min-length 7 \
+#     --sensitive \
+#     --rerun
 
 # Example short usage:~~~~
 # DATA_DIRECTORY="/home/armetcal/projects/rrg-bfinlay/armetcal/cmmi/pilot_g4h/mtx_extracted"
